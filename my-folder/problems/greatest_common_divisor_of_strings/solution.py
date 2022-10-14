e3116705d657f68db771c67bcea7766afbc32411
Andrés Coronel <78624635+ACR1209@users.noreply.github.com>
2022-10-14 01:44:29 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (python3)

---
+++ my-folder/problems/greatest_common_divisor_of_strings/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 == str2:
+            return str1 # solution trivial
+        
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        x = gcd(len(str1), len(str2))
+        
+        
+        return str1[:x] if str1[:x] == str2[:x] else ""
+        
