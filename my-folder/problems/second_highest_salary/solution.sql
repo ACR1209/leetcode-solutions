229359aac9ba3c03a37f495a5c7c499e650902d7
Andrés Coronel <78624635+ACR1209@users.noreply.github.com>
2023-04-13 01:44:35 +0000

    Sync LeetCode submission - Second Highest Salary (mysql)

---
+++ my-folder/problems/second_highest_salary/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    MAX(salary) as SecondHighestSalary
+FROM
+    Employee
+WHERE
+    salary NOT IN (
+        SELECT 
+            MAX(salary) 
+        FROM 
+            Employee
+    )
